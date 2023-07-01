4d9728b6c805d21d5d643e46951e4efa25a2970e
Bryan <limbryan12@gmail.com>
2023-07-01 10:23:29 +0800

    Final submission for practical 4

---
+++ app/src/main/java/sg/edu/np/mad/week2t04/practical3/ListActivity.java
@@ -0,0 +1,89 @@
+package sg.edu.np.mad.week2t04.practical3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import com.github.javafaker.Faker;
+import java.util.ArrayList;
+import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        List<User> userList = new ArrayList<>();
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        Random random = new Random();
+        Faker faker = new Faker();
+
+        for (int i = 0; i < 20; i++) {
+            String name = faker.name().firstName();
+            String description = "Description " + (i + 1);
+            boolean followed = random.nextBoolean();
+            int id = generateRandomNumber();
+
+            User user = new User(name, description,id,followed);
+            userList.add(user);
+        }
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        UserAdapter userAdapter = new UserAdapter(userList);
+        recyclerView.setAdapter(userAdapter);
+
+        userAdapter.setOnItemClickListener(new UserAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                User user = userList.get(position);
+
+                // Show AlertDialog with name
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
+                builder.setTitle("Profile");
+                builder.setMessage(user.getName() + user.getId());
+                builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // Launch MainActivity
+                        // Launch MainActivity and pass the user data
+                        Intent intent = new Intent(ListActivity.this, MainActivity.class);
+                        intent.putExtra("user", user);
+                        startActivity(intent);
+
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+    }
+
+    private int generateRandomNumber() {
+        // Generate a random integer between 100000000 and 999999999 (inclusive)
+        Random random = new Random();
+        return random.nextInt(900000000) + 100000000;
+    }
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/sg/edu/np/mad/week2t04/practical3/UserAdapter.java
@@ -0,0 +1,132 @@
+package sg.edu.np.mad.week2t04.practical3;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import sg.edu.np.mad.week2t04.practical3.R;
+import sg.edu.np.mad.week2t04.practical3.User;
+
+public class UserAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int VIEW_TYPE_DEFAULT = 0;
+    private static final int VIEW_TYPE_SPECIAL = 1;
+
+    private List<User> userList;
+    private OnItemClickListener listener;
+
+    public UserAdapter(List<User> userList) {
+        this.userList = userList;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        if (viewType == VIEW_TYPE_SPECIAL) {
+            View view = inflater.inflate(R.layout.item_user_7, parent, false);
+            return new SpecialViewHolder(view);
+        } else {
+            View view = inflater.inflate(R.layout.item_user, parent, false);
+            return new DefaultViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+        User user = userList.get(position);
+
+        if (viewHolder instanceof SpecialViewHolder) {
+            SpecialViewHolder holder = (SpecialViewHolder) viewHolder;
+            // Bind data to views in the special layout
+            holder.imageViewProfile.setImageResource(R.drawable.ic_launcher_foreground);
+            holder.textViewNameId.setText(user.getName() + "-" + user.getId());
+            holder.textViewDescription.setText(user.getDescription());
+        } else if (viewHolder instanceof DefaultViewHolder) {
+            DefaultViewHolder holder = (DefaultViewHolder) viewHolder;
+            // Bind data to views in the default layout
+            holder.imageViewProfile.setImageResource(R.drawable.ic_launcher_foreground);
+            holder.textViewNameId.setText(user.getName() + "-" + user.getId());
+            holder.textViewDescription.setText(user.getDescription());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        User user = userList.get(position);
+        int idLastDigit = user.getId() % 10;
+        if (idLastDigit == 7) {
+            return VIEW_TYPE_SPECIAL;
+        } else {
+            return VIEW_TYPE_DEFAULT;
+        }
+    }
+
+    public class DefaultViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageViewProfile;
+        TextView textViewNameId;
+        TextView textViewDescription;
+
+        public DefaultViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewProfile = itemView.findViewById(R.id.imageViewProfile);
+            textViewNameId = itemView.findViewById(R.id.textViewNameId);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+
+            // Set click listener for the profile image
+            imageViewProfile.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION && listener != null) {
+                        listener.onItemClick(position);
+                    }
+                }
+            });
+        }
+    }
+
+    public class SpecialViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageViewProfile;
+        TextView textViewNameId;
+        TextView textViewDescription;
+
+        public SpecialViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewProfile = itemView.findViewById(R.id.imageViewProfile);
+            textViewNameId = itemView.findViewById(R.id.textViewNameId);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+
+            // Set click listener for the profile image
+            imageViewProfile.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION && listener != null) {
+                        listener.onItemClick(position);
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+}
